4aafc4796831efa7e7f4da4de45c2ae085b4a414
repl.it user <replituser@example.com>
2020-11-18 16:02:01 +0000

    aggiunto shampoo_somma.py

---
+++ shampoo_somma.py
@@ -0,0 +1,17 @@
+values = []
+
+my_file = open("shampoo_sales.csv", "r")
+
+for line in my_file:
+
+    elements = line.split(',')
+    if elements[0] != 'Date':
+
+        date = elements[0]
+        value = elements[1]
+
+        values.append(float(value))
+
+sum_values = sum(values)
+
+print(sum_values)
